3f4501edcaed88c9a74ae518688d90bc5dd7dd8d
David\Dado <davidhancin4@gmail.com>
2021-01-06 11:08:00 +0100

    Dokončenie projektu

--- Bankovy system/Bankovy system.cpp
+++ Bankovy system/Bankovy system.cpp
@@ -28,17 +28,19 @@ public:
 	void report() const;
 	void dep(int);
 	int retacno() const;
-
+	void draw(int);
+	int retdeposit() const;
+	void showaccount() const;
 
 };
 
 void bank::openaccount()
 {
-	cout << "\n\n\tEnter Account Number: ";
+	cout << "\n\n\tZadajte číslo účtu: ";
 	cin >> anum;
-	cout << "\n\n\tEnter Name: ";
+	cout << "\n\n\tZadajte meno: ";
 	cin >> name;
-	cout << "\n\n\tEnter  Balance: ";
+	cout << "\n\n\tZadajte vklad: ";
 	cin >> balance;
 }
 
@@ -58,17 +60,35 @@ int bank::retacno() const
 	return anum;
 }
 
+void bank::draw(int x)
+{
+	balance -= x;
+}
+
+int bank::retdeposit() const
+{
+	return balance;
+}
+
+void bank::showaccount() const
+{
+	cout << "\nČíslo účtu : " << anum;
+	cout << "\nMeno majiteľa účtu : ";
+	cout << name;
+	cout << "\nZostatok : " << balance;
+}
 
 void create_account();
 void uvod();
 void display_all();
 void delete_account(int);
-
+void deposit_withdraw(int, int);
 
 //HLAVNA FUNKCIA 
 
 int main()
 {
+	setlocale(LC_ALL, "");
 	int n;
 	char ch;
 	uvod();
@@ -78,13 +98,13 @@ int main()
 
 		system("cls");
 		cout << "\n\n\t       MENU";
-		cout << "\n\n\t01. OPEN ACCOUNT" << endl;
-		cout << "\n\n\t02. CLOSE ACCOUNT" << endl;
-		cout << "\n\n\t03. SHOW ALL ACCOUNTS" << endl;
-		cout << "\n\n\t04. DEPOSIT IN ACCOUNT" << endl;
-		cout << "\n\n\t05. WITHDRAW FROM ACCOUNT" << endl;
-		cout << "\n\n\t06. EXIT" << endl;
-		cout << "\n\n\tSelect Your Option (1-6) " << endl;
+		cout << "\n\n\t01. Vytvoriť účet" << endl;
+		cout << "\n\n\t02. Zavrieť účet" << endl;
+		cout << "\n\n\t03. Zobraziť všetky účty" << endl;
+		cout << "\n\n\t04. Vklad na účet" << endl;
+		cout << "\n\n\t05. Výber z účtu" << endl;
+		cout << "\n\n\t06. Exit" << endl;
+		cout << "\n\n\tVyber možnosť (1-6) " << endl;
 		cin >> ch;
 		system("cls");
 		switch (ch)
@@ -93,7 +113,7 @@ int main()
 			create_account();
 			break;
 		case '2':
-			cout << "\n\n\tEnter The account Number : ";
+			cout << "\n\n\tZadajte číslo účtu : ";
 			cin >> n;
 			delete_account(n);
 			break;
@@ -102,13 +122,15 @@ int main()
 			display_all();
 			break;
 		case '4':
-			create_account();
+			cout << "\n\n\tZadajte číslo účtu : "; cin >> n;
+			deposit_withdraw(n, 1);
 			break;
 		case '5':
-			create_account();
+			cout << "\n\n\tZadajte číslo účtu : "; cin >> n;
+			deposit_withdraw(n, 2);
 			break;
 		case '6':
-			cout << "\n\n\tHave a nice day" << endl;
+			cout << "\n\n\tPekný deň" << endl;
 			break;
 
 		}
@@ -137,9 +159,9 @@ void display_all()
 	ifstream inFile;
 	inFile.open("account.dat", ios::binary);
 
-	cout << "\n\n\t\tACCOUNT HOLDER LIST\n\n";
+	cout << "\n\n\t\tZOZNAM UCTOV\n\n";
 	cout << "====================================================\n";
-	cout << "Ac.num.     NAME           Balance\n";
+	cout << "čis.uč.     Meno           Zostatok\n";
 	cout << "====================================================\n";
 	while (inFile.read(reinterpret_cast<char*> (&ac), sizeof(bank)))
 	{
@@ -157,7 +179,7 @@ void delete_account(int n)
 	inFile.open("account.dat", ios::binary);
 	if (!inFile)
 	{
-		cout << "\n\n\tThere are no accounts";
+		cout << "\n\n\tNeexistujú žiadne účty";
 		return;
 	}
 	outFile.open("Temp.dat", ios::binary);
@@ -173,5 +195,53 @@ void delete_account(int n)
 	outFile.close();
 	remove("account.dat");
 	rename("Temp.dat", "account.dat");
-	cout << "\n\n\tRecord Deleted ..";
+	cout << "\n\n\tZáznam bol odstránený";
+}
+
+
+void deposit_withdraw(int n, int option)
+{
+	int amt;
+	bool found = false;
+	bank ac;
+	fstream File;
+	File.open("account.dat", ios::binary | ios::in | ios::out);
+	if (!File)
+	{
+		cout << "File could not be open !! Press any Key...";
+		return;
+	}
+	while (!File.eof() && found == false)
+	{
+		File.read(reinterpret_cast<char*> (&ac), sizeof(bank));
+		if (ac.retacno() == n)
+		{
+			ac.showaccount();
+			if (option == 1)
+			{
+				cout << "\n\n\tVKLAD ";
+				cout << "\n\nZadajte čiastku ktorá sa má vložiť";
+				cin >> amt;
+				ac.dep(amt);
+			}
+			if (option == 2)
+			{
+				cout << "\n\n\tVYBER";
+				cout << "\n\nZadajte čiastku ktorá sa má vybrať";
+				cin >> amt;
+				int bal = ac.retdeposit() - amt;
+				ac.draw(amt);
+
+
+			}
+			int pos = (-1) * static_cast<int>(sizeof(ac));
+			File.seekp(pos, ios::cur);
+			File.write(reinterpret_cast<char*> (&ac), sizeof(bank));
+			cout << "\n\n\t Záznam bol aktualizovaný";
+			found = true;
+		}
+	}
+	File.close();
+	if (found == false)
+		cout << "\n\n Účet nebol nájdený ";
 }
